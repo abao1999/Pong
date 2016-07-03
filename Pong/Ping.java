d354457b0b5bc03ff3f6867a432ef9651d5c4712
Anthony Bao <anthonybao1999@gmail.com>
2016-07-02 19:29:55 -0700

    Update Ping.java

--- Pong/Ping.java
+++ Pong/Ping.java
@@ -1,66 +1,61 @@
-/*    */ package Pong;
-/*    */ 
-/*    */ public class Ping
-/*    */ {
-/*    */   double rx;
-/*    */   double ry;
-/*    */   double vx;
-/*    */   double vy;
-/*    */   double radius;
-/*    */ 
-/*    */   public Ping(boolean playComputer)
-/*    */   {
-/* 12 */     this.rx = 0.0D;
-/* 13 */     this.ry = 0.0D;
-/* 14 */     this.vx = 0.0D;
-/* 15 */     this.vy = -0.02D;
-/* 16 */     this.radius = 0.05D;
-/*    */   }
-/*    */ 
-/*    */   void bounceOffVerticalWall()
-/*    */   {
-/* 21 */     this.vx = (-this.vx);
-/*    */   }
-/*    */ 
-/*    */   void bounceOffHorizontalWall()
-/*    */   {
-/* 26 */     this.vy = (-this.vy);
-/*    */   }
-/*    */ 
-/*    */   public void move(boolean playComputer)
-/*    */   {
-/* 31 */     if (Math.abs(this.rx + this.vx) + this.radius > 1.0D) bounceOffVerticalWall();
-/* 32 */     if ((playComputer) && 
-/* 33 */       (this.ry + this.vy + this.radius > 1.0D)) bounceOffHorizontalWall();
-/*    */ 
-/* 35 */     this.rx += this.vx;
-/* 36 */     this.ry += this.vy;
-/*    */   }
-/*    */ 
-/*    */   public void draw()
-/*    */   {
-/* 41 */     StdDraw.filledCircle(this.rx, this.ry, this.radius);
-/*    */   }
-/*    */ 
-/*    */   public static void main(String[] args)
-/*    */   {
-/* 47 */     boolean playComputer = false;
-/* 48 */     Ping b1 = new Ping(playComputer);
-/*    */ 
-/* 50 */     StdDraw.setXscale(-1.0D, 1.0D);
-/* 51 */     StdDraw.setYscale(-1.0D, 1.0D);
-/*    */     while (true) {
-/* 53 */       StdDraw.setPenColor(StdDraw.BOOK_LIGHT_BLUE);
-/* 54 */       StdDraw.filledSquare(0.0D, 0.0D, 1.0D);
-/* 55 */       StdDraw.setPenColor(StdDraw.BLACK);
-/* 56 */       b1.move(playComputer);
-/* 57 */       b1.draw();
-/* 58 */       StdDraw.show(20);
-/*    */     }
-/*    */   }
-/*    */ }
+package Pong;
 
-/* Location:           /Users/timcoker/Desktop/Pong v1.0.jar
- * Qualified Name:     Pong.Ping
- * JD-Core Version:    0.6.0
- */
+public class Ping {
+
+    // instance variables
+     double rx, ry;        // position
+     double vx, vy;        // velocity
+     double radius;  // radius
+
+    // constructor
+    public Ping(boolean playComputer) {
+    	rx = 0;
+    	ry = 0;
+        vx = 0;
+        vy = -0.02;
+        radius = 0.05;
+    }
+
+    // bounce of vertical wall by reflecting x-velocity
+     void bounceOffVerticalWall() {
+        vx = -vx;
+    }
+
+    // bounce of horizontal wall by reflecting y-velocity
+     void bounceOffHorizontalWall() {
+        vy = -vy;
+    }
+
+    // move the ball one step
+    public void move(boolean playComputer) {
+        if (Math.abs(rx + vx) + radius > 1.0) bounceOffVerticalWall();
+        if (playComputer)	{
+        	if ((ry+vy)+radius>1.0) bounceOffHorizontalWall();
+        }
+        rx = rx + vx;
+        ry = ry + vy;
+    }
+
+    // draw the ball
+    public void draw() {
+        StdDraw.filledCircle(rx, ry, radius);
+    }
+    // test client
+    public static void main(String[] args) {
+
+        // create and initialize two balls
+    	boolean playComputer = false;
+        Ping b1 = new Ping(playComputer);
+        // animate them
+        StdDraw.setXscale(-1.0, +1.0);
+        StdDraw.setYscale(-1.0, +1.0);
+        while (true) {
+            StdDraw.setPenColor (StdDraw.BOOK_LIGHT_BLUE);
+            StdDraw.filledSquare(0.0, 0.0, 1.0);
+            StdDraw.setPenColor(StdDraw.BLACK);
+            b1.move(playComputer);
+            b1.draw();
+            StdDraw.show(20);
+        }
+    }
+}
